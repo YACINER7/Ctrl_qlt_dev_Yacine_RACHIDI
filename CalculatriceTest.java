7bfa783964754469509ca2004f001aa4165888fc
YacineR7 <rachidi.yacine7@gmail.com>
2025-05-23 10:51:17 +0200

    Création des fichiers initial et implémentation du fichier CalculatriceTest.java

---
+++ CalculatriceTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CalculatriceTest {
+
+    Calculatrice calculatrice = new Calculatrice();
+
+    @Test
+    public void addTest() {
+        assertEquals(2.0, calculatrice.add(1.0, 1.0));
+    }
+
+    @Test
+    public void multTest() {
+        assertEquals(8.0, calculatrice.mult(2.0, 4.0));
+    }
+
+    @Test
+    public void divTest() {
+        assertEquals(4.0, calculatrice.div(8.0, 2.0));
+    }
+
+    @Test
+    public void divParZeroTest() {
+        assertThrows(ArithmeticException.class, () -> {
+            calculatrice.div(7.0, 0.0);
+            });
+    }
+
+
+}
+
